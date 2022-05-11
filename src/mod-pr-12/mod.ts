f85dc6285462fdc60b67188dd6b7d04329233a33
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-11 11:00:13 +0100

    modi

--- src/mod-pr-12/mod.ts
+++ src/mod-pr-12/mod.ts
@@ -3,10 +3,10 @@ import {join} from 'path';
 import {spawn} from 'child_process';
 
 const app = express();
-
+console.log(__dirname);
 app.use(express.static(join(__dirname, './execmd')));
 
-app.get('/notes', (req, res) => {
+app.get('/execmd', (req, res) => {
   if (!req.query.cmd) {
     return res.send({
       error: 'A cmd has to be provided',
@@ -18,23 +18,50 @@ app.get('/notes', (req, res) => {
       error: 'Args has to be provided',
     });
   }
-
+  // Imprimimos la query.
   console.log(req.query);
+  // Hacemos spawn del comando.
   const command = spawn(`${req.query.cmd}`, [`${req.query.args}`]);
-  command.stdout.pipe(process.stdout);
-
-  res.send({
-    notes: [
-      {
-        cmd: `${req.query.cmd}`,
-        args: `${req.query.args}`,
-      }
-    ]
+  //command.stdout.pipe(process.stdout);
+  ////////////////////////////
+  let result: string = '';
+  command.stdout.on('data', (data) => {
+    result += data;
   });
+  command.on('close', () => {
+    console.log(`Result: ${result}`);
+    res.send({
+      execmd: [
+        {
+          cmd: `${req.query.cmd}`,
+          args: `${req.query.args}`,
+          output: `${result}`,
+        }
+      ]
+    });
+  })
+  command.on('error', (err) => {
+    res.send({
+      execmd: [
+        {
+          cmd: `Command error`,
+        }
+      ]
+    });
+  })
+  ///////////////////////////
+  // res.send({
+  //   execmd: [
+  //     {
+  //       cmd: `${req.query.cmd}`,
+  //       args: `${req.query.args}`,
+  //     }
+  //   ]
+  // });
 });
 
 app.get("*", function (_, res) {
-  res.send("Error_page 404");   
+  res.send('<h1>404</h1>');
 });
 
 app.listen(3000, () => {
